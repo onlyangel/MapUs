88ea7f618c6ce3054310f602bf9d1e10d11fbfe3
Jose Angel Espinoza Portillo <jose@ngel.es>
2012-09-06 16:47:58 -0500

    Agregando el agregar y jalar Posiciones

--- appengine/MapUs/golang/MapUs.go
+++ appengine/MapUs/golang/MapUs.go
@@ -6,11 +6,13 @@ import (
     "appengine"
     "appengine/datastore"
     "time"
+    "encoding/json"
 )
 
 func init() {
     //http.HandleFunc("/", home)
     http.HandleFunc("/ws/createSecionId", createSecionId)
+    http.HandleFunc("/ws/pushPos", pushPos)
     //http.HandleFunc("/ws/addList", listAdd)
     
 }
@@ -33,4 +35,61 @@ func createSecionId(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%d", ent.IdVal)
     return 
 
+}
+
+func pushPos(w http.ResponseWriter, r *http.Request) {
+    
+	data := r.FormValue("data")
+
+	//fmt.Fprintf(w, "%#v",data)
+
+	ent := new(Position)
+	json.Unmarshal([]byte(data), &ent)
+
+	ent.Creation = time.Now()
+
+	//fmt.Fprintf(w, "%#v",ent)
+
+	c := appengine.NewContext(r)
+	k := datastore.NewIncompleteKey(c, "Position", nil)
+
+	ent.Creation = time.Now()
+
+	key, err := datastore.Put(c, k, ent);
+	if  err != nil {
+       	http.Error(w, err.Error(), http.StatusInternalServerError)
+        return 
+    }
+	ent.IdVal = key.IntID()
+	datastore.Put(c,key,ent)
+
+	result := getPosiciones(r ,ent.Track)
+
+	salida,_ := json.Marshal(result)
+
+    fmt.Fprintf(w, "%s", salida)
+
+    return 
+
+}
+
+func getPosiciones(r *http.Request, id int64) []Position{
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Position").Filter("Track = ",id).Order("Creation")
+
+	result := make([]Position,0,0)
+	for t := q.Run(c); ; {
+        var x Position
+        _, err := t.Next(&x)
+        if err == datastore.Done {
+                break
+        }
+        if err != nil {
+                return nil
+        }
+        result = append(result, x)
+        
+    }
+
+    return result
 }
--- appengine/MapUs/golang/kindDB.go
+++ appengine/MapUs/golang/kindDB.go
@@ -8,3 +8,17 @@ type Track struct {
     IdVal int64
     Creation time.Time
 }
+
+type Position struct {
+    IdVal int64
+    Creation time.Time
+    Acuracy float32
+    Altitude float64
+    Latitude float64
+    Longitude float64
+    Speed float32
+    Time int64
+    Bearing float32
+
+    Track int64
+}
