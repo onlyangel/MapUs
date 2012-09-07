a352f716c557999c98e142de407c3f304a4ce13a
Jose Angel Espinoza Portillo <jose@ngel.es>
2012-09-07 13:31:10 -0500

    Ya sube la informacion de la posicion

--- Android/src/com/onlyangel/mapus/MainActivity.java
+++ Android/src/com/onlyangel/mapus/MainActivity.java
@@ -3,13 +3,7 @@ package com.onlyangel.mapus;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.TextView;
-
-import com.onlyangel.mapus.tools.NetworkTools;
 
 public class MainActivity extends Activity {
 	
@@ -22,11 +16,7 @@ public class MainActivity extends Activity {
         entity=this;
         
         this.startService(new Intent(this, UpdateService.class));
-        
-        NetworkTools nt = new NetworkTools();
-        
-		nt.fetchSecion("http://thegoapp.appspot.com/ws/createSecionId", handler);
-		
+
     }
     
     @Override	
@@ -35,19 +25,6 @@ public class MainActivity extends Activity {
     	super.onDestroy();
 	}
     
-    static Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message message) {
-        	String e = (String) message.obj;
-        	Log.i(e, MainActivity.class.toString());
-        	
-        	TextView tv = (TextView) MainActivity.entity.findViewById(R.id.base);
-        	tv.setText(e);
-        }
-    };
-    
-    
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
--- Android/src/com/onlyangel/mapus/UpdateService.java
+++ Android/src/com/onlyangel/mapus/UpdateService.java
@@ -1,17 +1,27 @@
 package com.onlyangel.mapus;
 
+import java.net.URLEncoder;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.onlyangel.mapus.tools.NetworkTools;
+
 public class UpdateService extends Service {
+	
+	public static final String PREFS_NAME = "MyPrefsFile";
 	String TAG = UpdateService.class.toString();
 	
 	LocationManager mlocManager;
@@ -27,16 +37,43 @@ public class UpdateService extends Service {
 		Toast.makeText(this, "My Service Created", Toast.LENGTH_SHORT).show();
 		Log.d(TAG, "onCreate");
 		
-		mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		mlocListener = new MyLocationListener();
-		mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,mlocListener);
+		NetworkTools nt = new NetworkTools();
+        
+		nt.fetchSecion("http://thegoapp.appspot.com/ws/createSecionId", handler);
+
 	}
+	
+	Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+        	String e = (String) message.obj;
+        	Log.i(e, MainActivity.class.toString());
+        	
+        	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("TrackId", e);
+            editor.commit();
+        	
+        	TextView tv = (TextView) MainActivity.entity.findViewById(R.id.base);
+        	tv.setText(e);
+        	
+        	mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    		mlocListener = new MyLocationListener();
+    		mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1,mlocListener);
+        }
+    };
 
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, "My Service Stopped", Toast.LENGTH_SHORT).show();
 		Log.d(TAG, "onDestroy");
-		mlocManager.removeUpdates(mlocListener);
+		if (mlocManager!= null)
+			mlocManager.removeUpdates(mlocListener);
+		
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("TrackId", "");
+        editor.commit();
 	}
 
 	@Override
@@ -48,8 +85,14 @@ public class UpdateService extends Service {
 
 	public class MyLocationListener implements LocationListener
 	{
+		private final class HandlerExtension extends Handler {
+		}
+
 		public void onLocationChanged(Location loc)
 		{
+			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		       String silent = settings.getString("TrackId", "");
+			String data = "{\"Acuracy\":"+loc.getAccuracy()+",\"Altitude\":"+loc.getAltitude()+",\"Latitude\":"+loc.getLatitude()+",\"Longitude\":"+loc.getLongitude()+",\"Speed\":"+loc.getSpeed()+",\"Time\":"+loc.getTime()+",\"Bearing\":"+loc.getBearing()+",\"Track\":"+silent+"}";
 			
 			loc.getLatitude();
 			loc.getLongitude();
@@ -62,6 +105,10 @@ public class UpdateService extends Service {
 			Toast.makeText( getApplicationContext(),
 					Text,
 					Toast.LENGTH_SHORT).show();
+			NetworkTools nt = new NetworkTools();
+			
+			nt.fetchSecion("http://thegoapp.appspot.com/ws/pushPos?data="+URLEncoder.encode(data),new HandlerExtension());
+			
 		}
 
 
